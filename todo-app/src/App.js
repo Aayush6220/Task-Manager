039c2a34f15d34124570dd5102588758489fc873
Aayush6220 <aayushrastogi06@gmail.com>
2026-06-10 11:10:43 +0530

    Adding Css
    Giving style to my page

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,30 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const[isCompleteScreen,setIsCompleteScreen]=useState(false);
   return (
     <div className="App">
      <h1>My Todos</h1>
      <div className='todo-wrapper'>
       <div className='todo-input'>
-        <div className='todo-input-itme'>
+        <div className='todo-input-item'>
           <label>Title</label>
           <input type="text" placeholder="What's the task title ? "/> 
         </div>
 
-        <div className='todo-input-itme'>
+        <div className='todo-input-item'>
           <label>Discription</label>
           <input type="text" placeholder="What's the task description ? "/> 
         </div>
 
-        <div className='todo-input-itme'>
+        <div className='todo-input-item'>
          <button type='button' className='primaryBtn'>Add</button>
         </div>
       </div>
       
       <div className='btn-area'>
-        <button>Todo</button>
-        <button>Completed</button>
+        <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`}
+         onClick={()=>setIsCompleteScreen(false)}>Todo</button>
+        <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`}
+        onClick={()=>setIsCompleteScreen(true)}>Completed</button>
       </div>
 
       <div className='todo-list'>
